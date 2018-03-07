dbe7e4eff9804801673353ae5fa2fe17e3768bc9
CraftedStone16 <stonedcraft16@gmail.com>
2018-03-07 14:14:23 -0600

    Update firespread.js

--- firespread.js
+++ firespread.js
@@ -1550,11 +1550,12 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
   if (oldUserChannel === undefined && newUserChannel !== undefined ) {
      client.channels.get(`${logs}`).send(`${newMember.user} joined a Voice Channel -=- ${newUserChannel.name}`)
     // User Joins a voice channel
-  } else if (newUserChannel !== oldUserChannel) {
-     client.channels.get(`${logs}`).send(`${newMember.user} joined a different Voice Channel -=- From ${oldUserChannel.name} to ${newUserChannel.name}`)
-    // User changes voice channels
   } else if (newUserChannel === undefined){
      client.channels.get(`${logs}`).send(`${newMember.user} left a Voice Channel -=- ${oldUserChannel.name}`)
     // User leaves a voice channel
+  }
+  if (newUserChannel !== oldUserChannel) {
+     client.channels.get(`${logs}`).send(`${newMember.user} joined a different Voice Channel -=- From ${oldUserChannel.name} to ${newUserChannel.name}`)
+    // User changes voice channels
   }
 });
