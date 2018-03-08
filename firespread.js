c077fc1a2c610ec68f6843f15b5431ab04a47c35
CraftedStone16 <stonedcraft16@gmail.com>
2018-03-08 12:55:55 -0600

    Update firespread.js

--- firespread.js
+++ firespread.js
@@ -1564,7 +1564,7 @@ client.on('roleCreate', role => {
 //   };
   
   client.channels.get(`${logs}`).send(`__**Role Created:**__\n• Name: ${role.name}\n• ID: ${role.id}\n• Color: ${role.hexColor}\n• Mentionable: ${role.mentionable}\n• Postion: ${role.calculatedPosition}`)
-  client.channels.get(`${logs}`).send(`__**Role Permissions**__ -- __**${role.name}**__\n\n${role.permissions.serialize()}`)
+  client.channels.get(`${logs}`).send(`__**Role Permissions**__ -- __**${role.name}**__\n\n${role.serialize()}`)
 });
 
 client.on('roleDelete', role => {
