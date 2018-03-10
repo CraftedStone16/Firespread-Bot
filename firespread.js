8ec06a56dbeab923e6bc2404f9bdcf62e415a445
CraftedStone16 <stonedcraft16@gmail.com>
2018-03-10 15:55:11 -0600

    Update firespread.js

--- firespread.js
+++ firespread.js
@@ -1676,11 +1676,10 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
 // });
 
 client.on('message', message => {
+  let args = message.content.split(' ').slice(1);
   if (message.content === '-tsay') {
-    let args = message.content.split(' ').slice(1);
     let tsay = args.join(' ');
     if(tsay < 1 ) return message.reply("Please provide a message!").then(message => message.delete(5000));
     message.channel.send(`${tsay}`);
-    return;
   }
 });
