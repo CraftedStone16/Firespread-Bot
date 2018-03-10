d7c37ecbe313af45f9d2de4583e72092316804c3
CraftedStone16 <stonedcraft16@gmail.com>
2018-03-10 15:40:29 -0600

    Update firespread.js

--- firespread.js
+++ firespread.js
@@ -1677,6 +1677,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
 
 client.on('message', message => {
   if (message.content === '-tsay') {
+    let args = message.content.split(' ').slice(1);
     let tsay = args.join(' ');
     if(tsay < 1 ) return message.reply("Please provide a message!").then(message => message.delete(5000));
     message.channel.send(`${tsay}`);
