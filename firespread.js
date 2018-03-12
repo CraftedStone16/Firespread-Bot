3a632c2f684619eb7dd1f0a790e46feead8e3033
CraftedStone16 <stonedcraft16@gmail.com>
2018-03-12 01:08:35 -0500

    Update firespread.js

--- firespread.js
+++ firespread.js
@@ -27,7 +27,7 @@ const queue = new Map();
 // exports.reload = reload;
 // process.env.MYSQL_HOST
 var con = mysql.createConnection({
-  host: sql9.freemysqlhosting.net,
+  host: process.env.MYSQL_HOST,
   user: sql9226104,
   password: Ib4WTDuNUI,
   database: sql9226104
