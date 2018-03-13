0ca1e1e0b7c9c146ffad2a1257ad60c797f3e642
CraftedStone16 <stonedcraft16@gmail.com>
2018-03-13 12:43:15 -0500

    Update firespread.js

--- firespread.js
+++ firespread.js
@@ -1582,21 +1582,21 @@ client.on('guildMemberRemove', member => {
 // Emoji Events
 client.on('emojiCreate', emoji => {
   let emojicreate = new Discord.RichEmbed()
-  .addField('\u200b', `The emoji **${emoji.name} was created! Animated: ${emoji.animted}`)
+  .addField(`-=- Emoji Event Log -=-`, `"**${emoji.name}**" was created!\nAnimated: ${emoji.animted}`)
   client.channels.get(`${logs}`).send(emojicreate)
  // client.channels.get(`${logs}`).send(`A new emoji was made: ${emoji.name}\nAnimated: ${emoji.animated}`)
 });
 
 client.on('emojiDelete', emoji => {
   let emojidelete = new Discord.RichEmbed()
-  .addField('\u200b', `The emoji **${emoji.name}** was deleted! Animated: ${emoji.animated}`)
+  .addField(`-=- Emoji Event Log -=-`, `"**${emoji.name}**" was deleted!\nAnimated: ${emoji.animated}`)
   client.channels.get(`${logs}`).send(emojidelete)
  // client.channels.get(`${logs}`).send(`An emoji was deleted: **${emoji.name}**`)
 });
 
 client.on('emojiUpdate', (oldEmoji, newEmoji) => {
   let emojiupdate = new Discord.RichEmbed()
-  .addField(`**An Emoji updated! (${oldEmoji.id})`, `The emoji **${oldEmoji.name}** was renamed to **${newEmoji.name}**. Animated: ${newEmoji.animated}`)
+  .addField(`-=- Emoji Event Log -=- (${oldEmoji.id})`, `"**${oldEmoji.name}**" was renamed to "**${newEmoji.name}**"\nAnimated: ${newEmoji.animated}`)
   client.channels.get(`${logs}`).send(emojiupdate)
  // client.channels.get(`${logs}`).send(`An Emoji\'s name was updated:\n**Old Name**: ${oldEmoji.name}\n**New Name**: ${newEmoji.name}`)
 });
